760bc6365a142f33fd763a7e768328aafa1dd15e
U-CHIMCHIM\bryn <bryn@chimchim.(none)>
2012-04-15 14:33:38 -0700

    Lots of dox, minified version, license

--- jQuery.scaleFrame.js
+++ jQuery.scaleFrame.js
@@ -1,3 +1,28 @@
+/**
+* jQuery.scaleFrame.js
+* https://github.com/BrynM/jQuery.scaleFrame.js
+*
+*******************************************************************************
+* LICENSE
+********************************************************************************
+* Copyright (C) 2012, Bryn P. Mosher (GPLv2)
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Library General Public License Version 2 as published by
+* the Free Software Foundation.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+* more details.
+*
+* You should have received a copy of the GNU Library General Public License
+* Version 2 along with this library named as gpl-2.0.txt; if not, you can
+* find it at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or write to
+* the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301, USA.
+*
+*/
 (function( jQ ) {
 	jQ.fn.scaleFrame = function( opts, ele, onlyFirst ) {
 		var classRgx = /\w/
